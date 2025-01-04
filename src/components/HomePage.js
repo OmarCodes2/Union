be10143a354f45f7f516900a4325fc12b8f0b974
OmarCodes2 <omarcodes2@gmail.com>
2025-01-04 14:01:50 -0500

    add home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import '../HomePage.css'
+import React from "react";
+import "../HomePage.css";
 
-const HomePage = () => {
+function HomePage() {
   return (
     <div className="home-container">
+      {/* Logo and title */}
       <div className="header">
-        <div className="logo">Union</div>
-        <div className="subtitle">Get 6 chances to guess the words.</div>
+        <img src="/logo.png" alt="Wordle Logo" className="wordle-logo" />
+        <h1 className="wordle-title">Union</h1>
       </div>
-      <button className="play-button">Play</button>
-      <div className="footer">
+
+      {/* Tagline */}
+      <div className="tagline-container">
+        <p className="tagline">Get 6 chances to guess</p>
+        <p className="tagline">the 2 words in the Venn.</p>
+      </div>
+      
+
+      {/* Button row for wider screens, or stacked on narrow screens */}
+      <div className="button-group">
+        <button className="btn play-btn">Play</button>
+      </div>
+
+      {/* Date, puzzle info, and editor */}
+      <div className="puzzle-info">
         <p>January 4, 2025</p>
         <p>No. 1295</p>
+        <p>Edited by Omar Bakr</p>
       </div>
     </div>
   );
-};
+}
 
 export default HomePage;
